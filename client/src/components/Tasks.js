555ed72cdff835964fe6c5b98ac647ddcae9dbad
Anne <annebstn22@hotmail.ca>
2024-02-24 20:49:47 +0100

    hierarchical tasks functions, but not with user

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -1,47 +1,106 @@
-import { useState } from 'react'
-import axios from "axios";
+import { useState } from 'react';
+import axios from 'axios';
 import TaskList from './TasksList';
 
 function Tasks(props) {
+  // State variables
+  const [view, setView] = useState('list'); // 'list', 'tasks', 'subtasks'
+  const [selectedListId, setSelectedListId] = useState(null);
+  const [selectedTaskId, setSelectedTaskId] = useState(null);
+  const [taskData, setTaskData] = useState({ tasks: [], lists: [] });
 
-  const [taskData, setTaskData] = useState({ tasks: [] })
+  // Function to fetch data from the server
   function getData() {
     axios({
-      method: "GET",
-      url:"/tasks",
+      method: 'GET',
+      url: '/tasks',
       headers: {
-        Authorization: 'Bearer ' + props.token
-      }
+        Authorization: 'Bearer ' + props.token,
+      },
     })
-    .then((response) => {
-      const res =response.data
-      res.access_token && props.setToken(res.access_token)
-      setTaskData(({
-        tasks: res.tasks}))
-    }).catch((error) => {
-      if (error.response) {
-        console.log(error.response)
-        console.log(error.response.status)
-        console.log(error.response.headers)
+      .then((response) => {
+        const res = response.data;
+        res.access_token && props.setToken(res.access_token);
+
+        // Set task data and default view to list
+        setTaskData({
+          tasks: res.tasks,
+          lists: res.lists,
+        });
+        setView('list');
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
         }
-    })}
+      });
+  }
+
+  // Function to handle click on a list
+  function onClickList(listId) {
+    setSelectedListId(listId);
+    setView('tasks'); // Switch to the tasks view
+  }
+
+  // Function to handle click on a task
+  function onClickTask(taskId) {
+    setSelectedTaskId(taskId);
+    setView('subtasks'); // Switch to the subtasks view
+  }
 
   return (
     <div className="Task">
+      {/* Button to fetch data */}
       <p>To get your task details: </p>
       <button onClick={getData}>Click me</button>
-      {taskData && (
+
+      {/* Display list names */}
+      {view === 'list' && (
         <div>
-          {typeof taskData.tasks === 'undefined' ? (
-            <p>No tasks</p>
-          ) : (
-            <TaskList tasks={taskData.tasks} />
-          )}
+          <p>Lists:</p>
+          <ul>
+            {taskData.lists.map((list) => (
+              <li key={list.list_id} onClick={() => onClickList(list.list_id)}>
+                {list.list_name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {/* Display highest level tasks */}
+      {view === 'tasks' && (
+        <div>
+          <p>Tasks</p>
+          <ul>
+            {taskData.tasks
+              .filter(
+                (task) => task.list_id === selectedListId && !task.parent_task_id
+              )
+              .map((task) => (
+                <li key={task.task_id} onClick={() => onClickTask(task.task_id)}>
+                  {task.task_title}
+                </li>
+              ))}
+          </ul>
+        </div>
+      )}
+
+      {/* Display subtasks */}
+      {view === 'subtasks' && (
+        <div>
+          <p>Subtasks:</p>
+          <TaskList
+            tasks={taskData.tasks.filter(
+              (task) => task.parent_task_id === selectedTaskId
+            )}
+          />
         </div>
       )}
     </div>
   );
-
 }
 
 export default Tasks;
