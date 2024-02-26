bf3be7f55080624f0a0f64a3911366915540f3fe
Anne <annebstn22@hotmail.ca>
2024-02-26 14:28:37 +0100

    Add space after "Completed"

--- client/src/components/TasksList.js
+++ client/src/components/TasksList.js
@@ -8,7 +8,7 @@ const TaskList = ({ tasks }) => {
         {tasks.map((task,i) => (
             <li key={task.task_id}>
             {i+1}. 
-            {task.done ? ' (Completed)' : ''}
+            {task.done ? ' (Completed) ' : ''}
             {task.task_title}
             {task.children && task.children.length > 0 && (
             <TaskList tasks={task.children} />
