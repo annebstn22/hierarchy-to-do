c38fd4bbaea2989102827b0d96534304ff78caab
Anne <annebstn22@hotmail.ca>
2024-02-29 11:29:57 +0000

    Edit button

--- client/src/components/TasksList.js
+++ client/src/components/TasksList.js
@@ -3,40 +3,12 @@ import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
 
-const TaskList = ({ tasks, onClickTask, token, refreshData}) => {
-  const [editingTasks, setEditingTasks] = useState([]);
-  const [newTaskTitles, setNewTaskTitles] = useState({});
+const TaskList = ({ tasks, onClickTask, token, refreshData }) => {
+  const [editingTaskId, setEditingTaskId] = useState(null);
 
   const handleCheckboxChange = (taskId, doneStatus) => {
-
-    console.log('Task ID:', taskId);
-    console.log('Done:', !doneStatus);
-
     const newData = {
-      done: !doneStatus
-    };
-
-    console.log('Request Payload:', newData);
-
-    axios
-      .put(`/tasks/${taskId}`, newData,{
-        headers: {
-          Authorization: `Bearer ${token}`
-        },
-      })
-      .then((response) => {
-        console.log('Task updated:', response.data);
-        // Refresh task data
-        refreshData();
-      })
-      .catch((error) => {
-        console.error('Error updating task', error);
-      });
-  };
-
-  const handleRenameTask = (taskId) => {
-    const newData = {
-      task_title: newTaskTitles[taskId],
+      done: !doneStatus,
     };
 
     axios
@@ -47,9 +19,26 @@ const TaskList = ({ tasks, onClickTask, token, refreshData}) => {
       })
       .then((response) => {
         refreshData();
-        setEditingTasks((prevEditingTasks) =>
-          prevEditingTasks.filter((id) => id !== taskId)
-        );
+      })
+      .catch((error) => {
+        console.error('Error updating task', error);
+      });
+  };
+
+  const handleRenameTask = (taskId, newTitle) => {
+    const newData = {
+      task_title: newTitle,
+    };
+
+    axios
+      .put(`/tasks/${taskId}`, newData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        refreshData();
+        setEditingTaskId(null);
       })
       .catch((error) => {
         console.error('Error renaming task', error);
@@ -64,8 +53,6 @@ const TaskList = ({ tasks, onClickTask, token, refreshData}) => {
         },
       })
       .then((response) => {
-        console.log('Task deleted:', response.data);
-        // Refresh task data
         refreshData();
       })
       .catch((error) => {
@@ -87,33 +74,24 @@ const TaskList = ({ tasks, onClickTask, token, refreshData}) => {
             <li>
               {task.done ? ' (Completed) ' : ''}
 
-              {!editingTasks.includes(task.task_id) && (
-                <div className="task-title" onClick={() => setEditingTasks((prev) => [...prev, task.task_id])}>
-                  <span>{task.task_title}</span>
-                </div>
-              )}
-
-              {editingTasks.includes(task.task_id) && (
+              {editingTaskId === task.task_id ? (
                 <form
                   onSubmit={(e) => {
                     e.preventDefault();
-                    handleRenameTask(task.task_id);
-                  }}>
-                  <input
-                    type="text"
-                    value={newTaskTitles[task.task_id] || task.task_title}
-                    onChange={(ev) =>
-                      setNewTaskTitles((prevNewTaskTitles) => ({
-                        ...prevNewTaskTitles,
-                        [task.task_id]: ev.target.value,
-                      }))
-                    }
-                  />
+                    handleRenameTask(task.task_id, e.target.title.value);
+                  }}
+                >
+                  <input type="text" name="title" defaultValue={task.task_title} />
                   <button type="submit">Save</button>
-              </form>
+                </form>
+              ) : (
+                <>
+                  <div className="task-title" onClick={() => onClickTask(task.task_id)}>
+                    <span>{task.task_title}</span>
+                  </div>
+                  <button onClick={() => setEditingTaskId(task.task_id)}>Edit</button>
+                </>
               )}
-
-              <button onClick={() => onClickTask(task.task_id)}>-&gt;</button>
               <button onClick={() => handleDeleteTask(task.task_id)}>x</button>
             </li>
           </div>
