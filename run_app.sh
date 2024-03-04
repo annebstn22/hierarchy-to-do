10ed07f5f9e31a68e3a311357007f56c4215e5c6
Anne <annebstn22@hotmail.ca>
2024-03-04 13:09:52 +0000

    Changed run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 # Backend setup
+cd flask-server
 python3 -m venv venv
 source venv/bin/activate
+
+# Install backend dependencies from the backend requirements.txt file
 pip3 install -r requirements.txt
+
+# Run the Flask app in the background
+nohup python3 app.py &
 
 # Initialize the database
 python3 init_db.py
 
-# Run the Flask app
-python3 app.py &
+cd ..
+
+# Install general dependencies from the root requirements.txt file
+pip3 install -r requirements.txt
 
 # Frontend setup
 cd client
-npm install
+npm install axios
 
 # Run the React app
 npm start
